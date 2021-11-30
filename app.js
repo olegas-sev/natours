a4116e875db9c4bafa1c2c263c31c1c628ed7f2a
Olegas Sevcenko <olegas.sevcenko@elev.ga.ntig.se>
2021-11-30 10:50:10 +0100

    Delete req

--- app.js
+++ app.js
@@ -81,6 +81,22 @@ app.patch('/api/v1/tours/:id', (req, res) => {
   })
 })
 
+app.delete('/api/v1/tours/:id', (req, res) => {
+  const tour = tours.find(tour => tour.id === req.params.id * 1)
+
+  if (!tour) {
+  return res.status(404).json({
+    satus: 'fail',
+    message: 'Invalid ID'
+  })
+  }
+
+  res.status(204).json({
+    status: 'success',
+    data: null
+  })
+})
+
 const port = 3000;
 
 app.listen(port, () => {
