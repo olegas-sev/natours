4e3f3d952df245eb10791cdecaae0c5b9f2620ea
olegas-sev <olegas.sevcenko@elev.ga.ntig.se>
2022-03-13 23:41:21 +0100

    Mongoose mongodb config

--- server.js
+++ server.js
@@ -1,10 +1,24 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const app = require('./app');
 // Start the server
 const port = process.env.PORT || 3000;
-
 dotenv.config({ path: './config.env' });
+
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then(() => console.log('DB Connection successfull!'));
 
 app.listen(port, () => {
   console.log(`Listening... http://localhost:${port}`);
+  console.log(process.env.DATABASE);
 });
