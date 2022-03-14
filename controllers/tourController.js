9a1cc1a284b4a086a0bef083bd8d8308d2e4ad1a
olegas-sev <olegas.sevcenko@elev.ga.ntig.se>
2022-03-14 22:39:23 +0100

    MVP Refactoring

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,10 +1,6 @@
-const fs = require('fs');
+const Tour = require('./../models/toursModel');
 
-// Read tours
-const tours = JSON.parse(
-  fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`)
-);
-
+/* 
 exports.checkID = (req, res, next) => {
   const tour = tours.find((tour) => tour.id === req.params.id * 1);
   if (!tour) {
@@ -14,61 +10,41 @@ exports.checkID = (req, res, next) => {
     });
   }
   next();
-};
-
-exports.checkBody = (req, res, next) => {
-  if (!req.body.name == '' || !req.body.price == '') {
-    return res.status(400).json({
-      status: 'fail',
-      message: 'Missing name or price',
-    });
-  }
-  next();
-};
+}; 
+*/
 
 // Route handlers
 exports.getAllTours = (req, res) => {
   res.status(200).json({
     status: 'success',
-    results: tours.length,
-    data: {
-      tours,
-    },
+    // results: tours.length,
+    // data: {
+    //   tours,
+    // },
   });
 };
 
 exports.getTour = (req, res) => {
+  console.log(req.params);
   const id = req.params.id * 1;
-  const tour = tours.find((el) => el.id === id);
-
-  res.status(200).json({
-    status: 'success',
-    results: tours.length,
-    data: {
-      tour,
-    },
-  });
+  // const tour = tours.find((el) => el.id === id);
+  // res.status(200).json({
+  //   status: 'success',
+  //   results: tours.length,
+  //   data: {
+  //     tour,
+  //   },
+  // });
 };
 
 exports.createTour = (req, res) => {
-  const newId = tours[tours.length - 1].id + 1;
-  const newTour = Object.assign({ id: newId }, req.body);
-  tours.push(newTour);
-  fs.writeFile(
-    `${__dirname}/dev-data/data/tours-simple.json`,
-    JSON.stringify(tours),
-    (err) => {
-      res.status(201).json({
-        status: 'success',
-        data: { tour: newTour },
-      });
-    }
-  );
+  res.status(201).json({
+    status: 'success',
+    // data: { tour: newTour },
+  });
 };
 
 exports.updateTour = (req, res) => {
-  const tour = tours.find((tour) => tour.id === req.params.id * 1);
-
   res.status(200).json({
     status: 'success',
     data: {
