bfdbc112d82a854ebf6b55f75a00ae8fc7ba9999
Olegas Sevcenko <olegas.sevcenko@elev.ga.ntig.se>
2022-03-24 14:56:13 +0100

    Implemented protected routes and error handling

--- models/userModel.js
+++ models/userModel.js
@@ -34,6 +34,7 @@ const userSchema = new mongoose.Schema({
       message: 'Passwords are not the same',
     },
   },
+  passwordChangedAt: Date,
 });
 
 userSchema.pre('save', async function (next) {
@@ -51,6 +52,22 @@ userSchema.methods.correctPassword = async function (
   userPassword
 ) {
   return await bcrypt.compare(candidatePassword, userPassword);
+};
+
+userSchema.methods.changedPasswordAfter = function (JWTTimestamp) {
+  if (this.passwordChangedAt) {
+    // Convert MongoDB Date format into jwt.iat format in order to compare
+    const changedTimestamp = parseInt(
+      this.passwordChangedAt.getTime() / 1000,
+      10
+    );
+
+    // 1000 < 2000 | means password was changed, returns true
+    return JWTTimestamp < changedTimestamp;
+  }
+
+  // Means password was NOT changed
+  return false;
 };
 
 const User = mongoose.model('User', userSchema);
--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -1,18 +1,20 @@
 const express = require('express');
 const router = express.Router();
 const tourController = require('../controllers/tourController');
+const authController = require('../controllers/authController');
 
 // router.param('id', tourController.checkID);
-
+// Alias
 router
   .route('/top-5-cheap')
   .get(tourController.aliasTopTours, tourController.getAllTours);
 router.route('/tour-stats').get(tourController.getTourStats);
 router.route('/monthly-plan/:year').get(tourController.getMonthlyPlan);
 
+// Routes
 router
   .route('/')
-  .get(tourController.getAllTours)
+  .get(authController.protect, tourController.getAllTours)
   .post(tourController.createTour);
 router
   .route('/:id')
