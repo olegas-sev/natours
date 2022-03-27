c500c5b8b8a0f0bf505ce676d368b67ee69b8bfc
Olegas Sevcenko <olegas.sevcenko@elev.ga.ntig.se>
2022-03-27 18:26:08 +0200

    Sending unfiltered signup on dev and cookies

--- controllers/authController.js
+++ controllers/authController.js
@@ -15,6 +15,19 @@ const signToken = (id) => {
 const createSendToken = (user, statusCode, res) => {
   const token = signToken(user._id);
 
+  const cookieOptions = {
+    expires: new Date(
+      Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
+    ),
+    httpOnly: true,
+  };
+  if (process.env.NODE_ENV === 'production') cookieOptions.secure = true;
+
+  res.cookie('jwt', token, cookieOptions);
+
+  // Remove/Dont show the password
+  user.password = undefined;
+
   res.status(statusCode).json({
     status: 'success',
     token,
@@ -25,13 +38,14 @@ const createSendToken = (user, statusCode, res) => {
 };
 
 exports.signup = catchAsync(async (req, res, next) => {
-  const newUser = await User.create({
+  let signupOptions = {
     name: req.body.name,
     email: req.body.email,
     password: req.body.password,
     passwordConfirm: req.body.passwordConfirm,
-    // passwordChangedAt: req.body.passwordChangedAt,
-  });
+  };
+  if (process.env.NODE_ENV === 'development') signupOptions = req.body;
+  const newUser = await User.create(signupOptions);
   createSendToken(newUser, 201, res);
 });
 
