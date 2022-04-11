a3efd1933a3f710e624944f3190714f85d1217f9
Olegas Sevcenko <sevcenko.olegas@gmail.com>
2022-04-12 00:25:25 +0200

    Created generic handlerFactory for all routes

---
+++ controllers/handlerFactory.js
@@ -0,0 +1,92 @@
+const catchAsync = require('./../utils/catchAsync')
+const AppError = require('./../utils/appError')
+const APIFeatures = require('./../utils/apiFeatures')
+
+exports.deleteOne = (Model) =>
+  catchAsync(async (req, res, next) => {
+    const doc = await Model.findByIdAndDelete(req.params.id)
+
+    if (!doc) {
+      return next(new AppError('No document found with specified id', 404))
+    }
+
+    res.status(204).json({
+      status: 'success',
+      data: null,
+    })
+  })
+exports.updateOne = (Model) =>
+  catchAsync(async (req, res, next) => {
+    const doc = await Model.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true,
+    })
+
+    if (!doc) {
+      return next(new AppError('No document found with specified id', 404))
+    }
+
+    res.status(200).json({
+      status: 'success',
+      data: {
+        data: doc,
+      },
+    })
+  })
+
+exports.createOne = (Model) =>
+  catchAsync(async (req, res, next) => {
+    const doc = await Model.create(req.body)
+
+    res.status(201).json({
+      status: 'success',
+      data: {
+        data: doc,
+      },
+    })
+  })
+
+exports.getOne = (Model, options) =>
+  catchAsync(async (req, res, next) => {
+    let query = Model.findById(req.params.id)
+    if (options) query.populate(options)
+    // .populate make a query for specified ID's in field guides and returns us Users then returns that query
+    const doc = await query
+
+    if (!doc) {
+      return next(new AppError('No document found with specified id', 404))
+    }
+
+    res.status(200).json({
+      status: 'success',
+      data: {
+        data: doc,
+      },
+    })
+  })
+
+exports.getAll = (Model) =>
+  catchAsync(async (req, res, next) => {
+    // To allow for nested GET reviews on tour (HACK)
+    let filter = {}
+    if (req.params.tourId) filter = { tour: req.params.tourId }
+
+    // EXECUTE a query
+    const features = new APIFeatures(Model.find(filter), req.query)
+      .filter()
+      .sort()
+      .limitFields()
+      .paginate()
+
+    const docs = await features.query
+
+    res.status(200).json({
+      status: 'success',
+      results: docs.length,
+      data: {
+        data: {
+          docs,
+        },
+      },
+    })
+  })
--- controllers/userController.js
+++ controllers/userController.js
@@ -1,26 +1,15 @@
-const User = require('./../models/userModel');
-const catchAsync = require('./../utils/catchAsync');
-const AppError = require('./../utils/appError');
+const User = require('./../models/userModel')
+const catchAsync = require('./../utils/catchAsync')
+const AppError = require('./../utils/appError')
+const factory = require('./handlerFactory')
 
 const filterObj = (obj, ...allowedFields) => {
-  const newObj = {};
+  const newObj = {}
   Object.keys(obj).forEach((el) => {
-    if (allowedFields.includes(el)) newObj[el] = obj[el];
-  });
-  return newObj;
-};
-
-exports.getAllUsers = catchAsync(async (req, res, next) => {
-  const users = await User.find();
-
-  res.status(200).json({
-    status: 'success',
-    results: users.length,
-    data: {
-      users,
-    },
-  });
-});
+    if (allowedFields.includes(el)) newObj[el] = obj[el]
+  })
+  return newObj
+}
 
 exports.updateMe = catchAsync(async (req, res, next) => {
   // 1) Create error if user POSTs password data
@@ -30,14 +19,14 @@ exports.updateMe = catchAsync(async (req, res, next) => {
         'This route is not for password updates. Please use /updateMyPassword',
         400
       )
-    );
+    )
   }
   // 2) Filter out unwanted field names that are not allowed to be updated
-  const filteredBody = filterObj(req.body, 'name', 'email');
+  const filteredBody = filterObj(req.body, 'name', 'email')
   const updatedUser = await User.findByIdAndUpdate(req.user.id, filteredBody, {
     new: true,
     runValidators: true,
-  });
+  })
   // await user.save(); // triggers validators
 
   // 3) Update user document
@@ -46,39 +35,27 @@ exports.updateMe = catchAsync(async (req, res, next) => {
     data: {
       user: updatedUser,
     },
-  });
-});
+  })
+})
 
 exports.deleteMe = catchAsync(async (req, res, next) => {
-  await User.findByIdAndUpdate(req.user.id, { active: false });
+  await User.findByIdAndUpdate(req.user.id, { active: false })
 
   res.status(204).json({
     status: 'success',
     data: null,
-  });
-});
+  })
+})
+
+exports.getAllUsers = factory.getAll(User)
+exports.getUser = factory.getOne(User)
+// DO NOT update password with this
+exports.updateUser = factory.updateOne(User)
+exports.deleteUser = factory.deleteOne(User)
 
 exports.createUser = (req, res) => {
   res.status(500).json({
     status: 'error',
-    message: 'This route is not yet defined!',
-  });
-};
-exports.getUser = (req, res) => {
-  res.status(500).json({
-    status: 'error',
-    message: 'This route is not yet defined!',
-  });
-};
-exports.updateUser = (req, res) => {
-  res.status(500).json({
-    status: 'error',
-    message: 'This route is not yet defined!',
-  });
-};
-exports.deleteUser = (req, res) => {
-  res.status(500).json({
-    status: 'error',
-    message: 'This route is not yet defined!',
-  });
-};
+    message: 'This route is not yet defined! Please use /signup instead!',
+  })
+}
