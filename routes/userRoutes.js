d2386f3a924c83872e3db3631d81484abb92949d
Olegas Sevcenko <sevcenko.olegas@gmail.com>
2022-04-12 00:27:05 +0200

    Nested routes for reviews and updated routes

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,32 +1,32 @@
-const express = require('express');
-const userController = require('../controllers/userController');
-const authController = require('../controllers/authController');
+const express = require('express')
+const userController = require('../controllers/userController')
+const authController = require('../controllers/authController')
 
 // Route mounting
-const router = express.Router();
+const router = express.Router()
 
-router.post('/signup', authController.signup);
-router.post('/login', authController.login);
+router.post('/signup', authController.signup)
+router.post('/login', authController.login)
 
-router.post('/forgotPassword', authController.forgotPassword);
-router.patch('/resetPassword/:token', authController.resetPassword);
+router.post('/forgotPassword', authController.forgotPassword)
+router.patch('/resetPassword/:token', authController.resetPassword)
 
 router.patch(
   '/updateMyPassword',
   authController.protect,
   authController.updatePassword
-);
-router.patch('/updateMe', authController.protect, userController.updateMe);
-router.delete('/deleteMe', authController.protect, userController.deleteMe);
+)
+router.patch('/updateMe', authController.protect, userController.updateMe)
+router.delete('/deleteMe', authController.protect, userController.deleteMe)
 
 router
   .route('/')
   .get(userController.getAllUsers)
-  .post(userController.createUser);
+  .post(userController.createUser)
 router
   .route('/:id')
   .get(userController.getUser)
   .patch(userController.updateUser)
-  .delete(userController.deleteUser);
+  .delete(userController.deleteUser)
 
-module.exports = router;
+module.exports = router
