5c8887628a3d83269f16ac9e03f286b7f78db75d
Olegas Sevcenko <sevcenko.olegas@gmail.com>
2022-04-15 13:53:51 +0200

    Protecting and restricting correct routes

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -11,19 +11,16 @@ router.post('/login', authController.login)
 router.post('/forgotPassword', authController.forgotPassword)
 router.patch('/resetPassword/:token', authController.resetPassword)
 
-router.patch(
-  '/updateMyPassword',
-  authController.protect,
-  authController.updatePassword
-)
-router.get(
-  '/me',
-  authController.protect,
-  userController.getMe,
-  userController.getUser
-)
-router.patch('/updateMe', authController.protect, userController.updateMe)
-router.delete('/deleteMe', authController.protect, userController.deleteMe)
+// ALL routes after this middleware require authorization, they are protected
+router.use(authController.protect)
+
+router.patch('/updateMyPassword', authController.updatePassword)
+router.get('/me', userController.getMe, userController.getUser)
+router.patch('/updateMe', userController.updateMe)
+router.delete('/deleteMe', userController.deleteMe)
+
+// Only admins have access to managing accounts
+router.use(authController.restrictTo('admin'))
 
 router
   .route('/')
