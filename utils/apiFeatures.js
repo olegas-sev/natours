61315d869f91c3c869ff3d521873159fe30361b7
Olegas Sevcenko <sevcenko.olegas@gmail.com>
2022-04-15 15:28:55 +0200

    JSDoc

--- utils/apiFeatures.js
+++ utils/apiFeatures.js
@@ -1,52 +1,70 @@
+const { Model } = require('mongoose')
+
+/**
+ * Quering features for the API
+ */
 class APIFeatures {
+  /**
+   * A thenable query object from Mongoose async operations.
+   * @typedef {object} Query
+   */
+
+  /**
+   * Create a query
+   * @param {Query} query - the query object that comes from Model.find()
+   * @param {string} queryString - the query that comes from request
+   */
   constructor(query, queryString) {
-    this.query = query;
-    this.queryString = queryString;
+    this.query = query
+    this.queryString = queryString
   }
 
   filter() {
     // 1A) Filtering
-    const queryObj = { ...this.queryString };
-    const excludedFields = ['page', 'sort', 'limit', 'fields'];
-    excludedFields.forEach((field) => delete queryObj[field]);
+    const queryObj = { ...this.queryString }
+    const excludedFields = ['page', 'sort', 'limit', 'fields']
+    excludedFields.forEach((field) => delete queryObj[field])
     // 1B) Advanced filtering (operators)
-    let queryStr = JSON.stringify(queryObj);
-    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
+    let queryStr = JSON.stringify(queryObj)
+    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`)
 
     // QUERY/FILTER
-    this.query = this.query.find(JSON.parse(queryStr));
-    return this;
+    this.query = this.query.find(JSON.parse(queryStr))
+    return this
   }
+
   sort() {
     // 2) Sorting
     if (this.queryString.sort) {
-      const sortBy = this.queryString.sort.split(',').join(' ');
-      this.query = this.query.sort(sortBy);
+      const sortBy = this.queryString.sort.split(',').join(' ')
+      this.query = this.query.sort(sortBy)
     } else {
-      this.query = this.query.sort('-createdAt');
+      this.query = this.query.sort('-createdAt')
     }
-    return this;
+    return this
   }
+
   limitFields() {
     // 3) Field limiting
     if (this.queryString.fields) {
-      const fields = this.queryString.fields.split(',').join(' ');
+      const fields = this.queryString.fields.split(',').join(' ')
       // Projection queries
-      this.query = this.query.select(fields);
+      this.query = this.query.select(fields)
     } else {
       // exclude unnecessary __v field by mongodb
-      this.query = this.query.select('-__v');
+      this.query = this.query.select('-__v')
     }
-    return this;
+    return this
   }
+
   paginate() {
     // 4) Pagination
-    const page = this.queryString.page * 1 || 1;
-    const limit = this.queryString.limit * 1 || 100;
-    const skip = (page - 1) * limit;
-    this.query = this.query.skip(skip).limit(limit);
+    const page = this.queryString.page * 1 || 1
+    const limit = this.queryString.limit * 1 || 100
+    const skip = (page - 1) * limit
+    this.query = this.query.skip(skip).limit(limit)
 
-    return this;
+    return this
   }
 }
-module.exports = APIFeatures;
+module.exports = APIFeatures
